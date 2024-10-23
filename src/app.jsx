ab8f3ef34b7985d5a305b093727a3c9a06561857
Ellie <elinorj@byu.edu>
2024-10-23 10:50:37 -0600

    moving it into the body

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,11 @@ import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 function App () {
     return (
         <BrowserRouter>
-        <div>
+        <div className="body">
             <header>
                 <nav className="navbar navbar-expand-lg" id="head-nav">
                     <div className="container-fluid">
-                        <img className="navbar-brand" id="logo" src="/Images/Logo.png" alt="logo"></img>
+                        <img className="navbar-brand" id="logo" src="/logo.png" alt="logo"></img>
                         <menu className="navbar-nav">
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="">Login</NavLink> 
@@ -27,24 +27,24 @@ function App () {
                     </div>
                 </nav>
             </header>
+
+            <Routes>
+                <Route
+                    path='/'
+                    element={<Login />}
+                    exact
+                />
+                <Route path="/OtherReviews" element={<OtherReviews />} />
+                <Route path="/MyReviews" element={<MyReviews />} />
+            </Routes>
+
+            <footer>
+                <div className="container-fluid">
+                    <h4 id="author">Author: Elinor Clark</h4>
+                    <h4 id="github"><a href="https://github.com/elliejeanclark/startup">GitHub</a></h4>
+                </div>
+            </footer>
         </div>
-
-        <Routes>
-            <Route
-                path='/'
-                element={<Login />}
-                exact
-            />
-            <Route path="/OtherReviews" element={<OtherReviews />} />
-            <Route path="/MyReviews" element={<MyReviews />} />
-        </Routes>
-
-        <footer>
-            <div className="container-fluid">
-                <h4 id="author">Author: Elinor Clark</h4>
-                <h4 id="github"><a href="https://github.com/elliejeanclark/startup">GitHub</a></h4>
-            </div>
-        </footer>
     </BrowserRouter>
     );
 }
