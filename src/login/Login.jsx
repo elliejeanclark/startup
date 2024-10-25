baabfe025a74b5a79d757723b4258e943033bc4e
Ellie <elinorj@byu.edu>
2024-10-25 11:47:22 -0600

    updated authstate from simon and login has a form

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 export function Login() {
-    return (
-        
-        
+    return (    
         <main>
             <div id="title">
                 <h1>Welcome to Pop In!</h1>
             </div>
             <div>
-                <h2>Please log in to continue</h2>
+                <form id="login-form">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                    <button type="submit">Login</button>
+                </form>
             </div>
         </main>
     );
