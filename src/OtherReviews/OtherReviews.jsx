f563a12d28e82eb7433672a6386575698e51d6de
Ellie <elinorj@byu.edu>
2024-10-26 19:54:56 -0600

    rating reviews is now working

--- src/OtherReviews/OtherReviews.jsx
+++ src/OtherReviews/OtherReviews.jsx
@@ -3,21 +3,38 @@ import './OtherReviews.css';
 
 export function OtherReviews() {
   const [review1, setIsRated1] = React.useState(false);
+  const [buttonLabel1, setButton1Label] = React.useState("Submit");
+
   const [review2, setIsRated2] = React.useState(false);
+  const [buttonLabel2, setButton2Label] = React.useState("Submit");
+
   const [review3, setIsRated3] = React.useState(false);
+  const [buttonLabel3, setButton3Label] = React.useState("Submit");
 
   const handleRating1 = (event) => {
     event.preventDefault();
+    const rangeVale = document.getElementById("rating-1").value;
+    setButton1Label('You rated this review a ' + rangeVale);
+    const currentRatingValue = document.getElementById("current-rating-1").value;
+    document.getElementById("current-rating-1").value = rangeVale;
     setIsRated1(true);
   }
 
   const handleRating2 = (event) => {
     event.preventDefault();
+    const rangeVale = document.getElementById("rating-2").value;
+    setButton2Label('You rated this review a ' + rangeVale);
+    const currentRatingValue = document.getElementById("current-rating-2").value;
+    document.getElementById("current-rating-2").value = rangeVale;
     setIsRated2(true);
   }
 
   const handleRating3 = (event) => {
     event.preventDefault();
+    const rangeVale = document.getElementById("rating-3").value;
+    setButton3Label('You rated this review a ' + rangeVale);
+    const currentRatingValue = document.getElementById("current-rating-3").value;
+    document.getElementById("current-rating-3").value = rangeVale;
     setIsRated3(true);
   }
   
@@ -35,11 +52,11 @@ export function OtherReviews() {
             <textarea className="review-text" id="recent-review-1-text" name="review" placeholder="Most recent review will be here"></textarea>
                         
             <label>What do you rate this review out of 10?</label>
-            <input type="range" id="rating-1" name="rating" min="0" max="10" />
-            <input className="btn" id="submit-review-rating" type="button" value="Submit" />
+            <input type="range" id="rating-1" name="rating" min="0" max="10" disabled={review1} />
+            <input className="btn" id="submit-review-rating" type="button" value={buttonLabel1} onClick={handleRating1} disabled={review1} />
                         
             <label>Current rating:</label>
-            <input onClick={handleRating1} className="current-rating" type="text" id="current-rating-1" name="rating" placeholder="Current rating here" readOnly />
+            <input className="current-rating" type="text" id="current-rating-1" name="rating" placeholder="Current rating here" readOnly />
           </div>
             
           <div id="recent-review-2">
@@ -47,8 +64,8 @@ export function OtherReviews() {
             <textarea className="review-text" id="recent-review-2-text" name="review" placeholder="Second most recent review will be here"></textarea>
                         
             <label>What do you rate this review out of 10?</label>
-            <input type="range" id="rating-2" name="rating" min="0" max="10" />
-            <input onClick={handleRating2} className="btn" id="submit-review-rating" type="button" value="Submit" />
+            <input type="range" id="rating-2" name="rating" min="0" max="10" disabled={review2} />
+            <input className="btn" id="submit-review-rating" type="button" value={buttonLabel2} onClick={handleRating2} disabled={review2} />
         
             <label>Current rating:</label>
             <input className="current-rating" type="text" id="current-rating-2" name="rating" placeholder="Current rating here" readOnly />
@@ -59,8 +76,8 @@ export function OtherReviews() {
             <textarea className="review-text" id="recent-review-3-text" name="review" placeholder="Third most recent review will be here"></textarea>
         
             <label>What do you rate this review out of 10?</label>
-            <input type="range" id="rating-3" name="rating" min="0" max="10" />
-            <input onClick={handleRating3} className="btn" id="submit-review-rating" type="button" value="Submit" />
+            <input type="range" id="rating-3" name="rating" min="0" max="10" disabled={review3} />
+            <input className="btn" id="submit-review-rating" type="button" value={buttonLabel3} onClick={handleRating3} disabled={review3} />
         
             <label>Current rating:</label>
             <input className="current-rating" type="text" id="current-rating-3" name="rating" placeholder="Current rating here" readOnly />
