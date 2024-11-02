cbe55fcd8d6839f1df980f70f071a448eee5d46f
Ellie <elinorj@byu.edu>
2024-11-02 17:49:01 -0600

    adding more of the backbone of index.js

--- service/index.js
+++ service/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
 
+let users = {};
+let reviews = {};
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
