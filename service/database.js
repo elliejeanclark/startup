105ed2a8541c110a3f6db5525e27885aae17c066
Ellie <elinorj@byu.edu>
2024-11-09 16:32:29 -0700

    starting to add basic mongo functionality

--- service/database.js
+++ service/database.js
@@ -1,2 +1,30 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const userCollection = db.collection('users');
+
+function getUser(username) {
+    return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(username, password) {
+    //hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = {
+        username: username,
+        password: hashedPassword,
+        token: uuid.v4()
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+}
